bc638789c1e16caddbc9d8dc80c9ef79b180a468
zhusitao <zhusitao1990@163.com>
2022-12-03 22:47:24 +0800

    c syntax

---
+++ data_structure.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+
+int main()
+{
+	// int
+	int a = 1;
+	int b = 2;
+	int sum = a + b;
+	printf("sum=%d\n", sum);
+	// long
+	long c = 123456;
+	printf("c=%ld\n", c);
+	// char
+	char DNA[] = "ATGC";
+	printf("DNA=%s\n", DNA);
+	return 0;
+}
---
+++ loop_condition.c
@@ -0,0 +1,10 @@
+int main()
+{
+    //loop
+    int a = 10;
+    for (a=0;a<100;a++)
+    {
+        printf("a=%d\n", a);
+    }
+    return 0;
+}
