cf311ea5742af9d3c721b41d853cee9d795f0546
Sitao Zhu <zhusitao1990@163.com>
2024-01-04 17:47:27 +0800

    Create data_structure.cpp

---
+++ data_structure.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    // int
+    int a = 1;
+    int b = 2;
+    int sum = a + b;
+    std::cout << "sum=" << sum << std::endl;
+    
+    // long
+    long c = 123456;
+    std::cout << "c=" << c << std::endl;
+    
+    // char
+    std::string DNA = "ATGC";
+    std::cout << "DNA=" << DNA << std::endl;
+    
+    return 0;
+}
+
