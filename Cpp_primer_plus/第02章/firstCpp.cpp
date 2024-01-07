72bbed3e9e292e92f10944db80409d3bd5c8ab3e
Sitao Zhu <zhusitao1990@163.com>
2024-01-07 10:39:34 +0800

    Create firstCpp.cpp

---
+++ Cpp_primer_plus/第02章/firstCpp.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // 导入头文件
+
+int main() {                                 // 函数头
+  using namespace std;                       // 使用std的命名空间中的函数
+  cout << "Come up and C++  me some time ."; // 输出，;是必须的，不能省略
+  cout << endl;                              // 换行
+  cout << "You won't regret it!" << endl;    // 输出
+  return 0;                                  // main 函数终止
+}                                            // 函数主体模块
