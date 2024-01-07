5aa9520b21b600ced1320c2c46facc94c9274816
Sitao Zhu <zhusitao1990@163.com>
2024-01-07 11:04:38 +0800

    Update firstCpp.cpp

--- Cpp_primer_plus/第02章/firstCpp.cpp
+++ Cpp_primer_plus/第02章/firstCpp.cpp
@@ -1,6 +1,7 @@
 #include <iostream> // 导入头文件
 
-int main() {                                 // 函数头
+int main()                                   // 函数头
+{                                            // 函数主题
   using namespace std;                       // 使用std的命名空间中的函数
   cout << "Come up and C++  me some time ."; // 输出，;是必须的，不能省略
   cout << endl;                              // 换行
