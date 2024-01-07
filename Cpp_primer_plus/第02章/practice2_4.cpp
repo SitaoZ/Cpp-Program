ec1b8ca652d150e3efb55f33696f7fcedbd7f68d
Sitao Zhu <zhusitao1990@163.com>
2024-01-07 14:50:52 +0800

    Create practice2_4.cpp

---
+++ Cpp_primer_plus/第02章/practice2_4.cpp
@@ -0,0 +1,15 @@
+// Created bu Sitao Zhu on 07/01/2024
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int years, months;
+  cout << "Please enter your age: ";
+  cin >> years;
+  months = years * 12;
+  cout << years << " years has " << months << " months." << endl;
+  return 0;
+}
