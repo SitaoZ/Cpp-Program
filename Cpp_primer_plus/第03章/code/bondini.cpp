478d1afd160a6df0ab1dc7a8b495e171c85b90dc
Sitao Zhu <zhusitao1990@163.com>
2024-01-07 21:13:35 +0800

    Create bondini.cpp

---
+++ Cpp_primer_plus/第03章/code/bondini.cpp
@@ -0,0 +1,13 @@
+// Created Sitao Zhu on 07/01/2024
+
+#include <iostream>
+int main() {
+  using namespace std;
+  cout << "\aOperation \"HyperHype\" is now activated!\n";
+  cout << "Enter you agent code:_____\b\b\b\b\b\b\b\b";
+  long code;
+  cin >> code;
+  cout << "\aYou entered " << code << "...\n";
+  cout << "\aCode verified! Proceed with Plan Z3!\n";
+  return 0;
+}
