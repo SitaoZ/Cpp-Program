5a1150c77ef0fe58d665b2fbfe6f997cce5cf888
Sitao Zhu <zhusitao1990@163.com>
2024-01-07 14:41:18 +0800

    Create practice_2_3.cpp

---
+++ Cpp_primer_plus/第02章/practice_2_3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void mice(){
+  cout << "Three blind mice" << endl;
+}
+
+void run(){
+  cout << "See how they run." << endl;
+}
+
+int main() 
+{
+  mice();
+  mice();
+  run();
+  run();
+  return 0;
+}
