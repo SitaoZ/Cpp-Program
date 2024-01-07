63282b99f7f55f7dc1f027bf9ca052b72607f883
Sitao Zhu <zhusitao1990@163.com>
2024-01-07 11:30:31 +0800

    Create practice1.cpp

---
+++ Cpp_primer_plus/第02章/practice1.cpp
@@ -0,0 +1,10 @@
+// Created by Sitao Zhu on 07/01/2024
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  cout << "Hi, I'm zhusitao and from Wuhan, Hubei, China." << endl;
+  return 0;
+}
