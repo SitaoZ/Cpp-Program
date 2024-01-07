dfb97083bde1c0272ade843ea7f4773bdeafa138
Sitao Zhu <zhusitao1990@163.com>
2024-01-07 11:37:32 +0800

    Create practice2.cpp

---
+++ Cpp_primer_plus/第02章/practice2.cpp
@@ -0,0 +1,15 @@
+// Create by Sitao Zhu on 07/01/2024
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int distance;
+  int yard;
+  cout << "Please enter the long: " << endl;
+  cin >> distance;
+  yard = 120 * distance;
+  cout << distance << " long equal to " << yard << " yard!" << endl;
+  return 0;
+}
