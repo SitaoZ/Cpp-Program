bc414c5ceca7849e02444699f446609dedb6356b
Sitao Zhu <zhusitao1990@163.com>
2024-01-08 20:27:56 +0800

    Update practice3_1.cpp

--- Cpp_primer_plus/第03章/code/practice3_1.cpp
+++ Cpp_primer_plus/第03章/code/practice3_1.cpp
@@ -1,1 +1,22 @@
-// Create by Sitao Zhu 
+// Create by Sitao Zhu on 08/01/2024
+
+#include <iostream>
+using namespace std;
+
+const int foot2inch = 12;
+
+int main() 
+{
+  int height = 0;
+  cout << "Please enter your height in inch: __\b\b";
+  cin >> height;
+
+  int foot = height / foot2inch;
+  float inch = height % foot2inch;
+  cout << "Your height in inch is: " << height 
+         << "; transforming in foot and inch is: " 
+         << foot << " foot "
+         << inch << " inch." << endl;
+
+    return 0;
+}
