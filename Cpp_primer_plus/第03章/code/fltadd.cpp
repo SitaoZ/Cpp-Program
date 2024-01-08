b969d33e34510162ef77f0c5ac903d40ba49c5a6
Sitao Zhu <zhusitao1990@163.com>
2024-01-08 15:41:02 +0800

    Update fltadd.cpp

--- Cpp_primer_plus/第03章/code/fltadd.cpp
+++ Cpp_primer_plus/第03章/code/fltadd.cpp
@@ -9,7 +9,7 @@ int main()
   float b = a + 1.0f;
 
   cout << "a = " << a << endl;
-  cout << "b - a = " << b - a << endl;
+  cout << "b - a = " << b - a << endl; // 结果为0，因为float只表示小数点后6或7位，原先a小数点后面有23位，因此为0；
   return 0;
   
 }
