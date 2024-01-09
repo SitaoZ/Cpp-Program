6bc4b0b0aa599cdf2802ed22dad64d8695d74145
Sitao Zhu <zhusitao1990@163.com>
2024-01-09 16:52:11 +0800

    Create arrayone.cpp

---
+++ Cpp_primer_plus/第04章/code/arrayone.cpp
@@ -0,0 +1,15 @@
+// Created by Sitao Zhu on 09/01/2024
+#include <iostream>
+
+int main()
+{
+  using namespace std;
+  int yams[3];
+  yams[0] = 7;
+  yams[1] = 8;
+  yams[2] = 6;
+
+  int yamcosts[3] = {20, 30, 5};
+  return 0;
+  
+}
