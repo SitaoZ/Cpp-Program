99df7da624e078ad844681871b027615e09071da
Sitao Zhu <zhusitao1990@163.com>
2024-01-16 09:12:58 +0800

    Create strtype1.cpp

---
+++ Cpp_primer_plus/第04章/code/strtype1.cpp
@@ -0,0 +1,18 @@
+/* Created by Sitao Zhu on 16-01-2024
+* 使用C++的string类
+*/
+  
+#include <iostream>
+#include <string> // 导入类
+
+
+int main()
+{
+  using namespace std;
+  char cahrr1[20]; 
+  char charr2[20] = "jaguar";
+  string str1;
+  string str2 = "panther"; 
+  
+  
+}
