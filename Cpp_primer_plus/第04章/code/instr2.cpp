deba9bf4abf8aabd1cd7d05834f400538889c9b5
Sitao Zhu <zhusitao1990@163.com>
2024-01-16 08:51:18 +0800

    Create instr2.cpp

---
+++ Cpp_primer_plus/第04章/code/instr2.cpp
@@ -0,0 +1,19 @@
+// Created by Sitzo Zhu on 16-01-2024
+
+#include <iostream>
+
+int main()
+{
+  using namespace std;
+  const int ArSize = 20;
+  char name[ArSize];
+  char dessert[ArSize];
+
+  cout << "Enter your name: \n";
+  cin.getline(name, ArSize); // 读取整行
+  cout << "Enter your favourite dessert:\n";
+  cin.getline(dessert, ArSize); // 读取第二行
+  cout << "I have some delicious " << dessert;
+  cout << " for you, " << name << ".\n";
+  return 0;
+}
